d0b4041674b3d27f2148333ae553542e3adfb421
allowMultiQueries <asd1161575755@outlook.com>
2025-01-04 23:54:13 +0800

    Update index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(() => {
     // Set default headers for AJAX requests
     $.ajaxSetup({
         headers: {
@@ -7,8 +7,8 @@ $(document).ready(function() {
     });
 
     // Function to handle submission of license information
-    window.submitLicenseInfo = function () {
-        let licenseInfo = {
+    window.submitLicenseInfo = () => {
+        const licenseInfo = {
             licenseeName: $('#licenseeName').val(),
             assigneeName: $('#assigneeName').val(),
             expiryDate: $('#expiryDate').val()
@@ -18,24 +18,24 @@ $(document).ready(function() {
     };
 
     // Function to handle search input
-    $('#search').on('input', function(e) {
-        $("#product-list").load('/search?search=' + e.target.value);
+    $('#search').on('input', (e) => {
+        $("#product-list").load(`/search?search=${e.target.value}`);
     });
 
     // Function to show license form
-    window.showLicenseForm = function () {
-        let licenseInfo = JSON.parse(localStorage.getItem('licenseInfo'));
-        $('#licenseeName').val(licenseInfo?.licenseeName || 'acme');
-        $('#assigneeName').val(licenseInfo?.assigneeName || '拒绝者');
-        $('#expiryDate').val(licenseInfo?.expiryDate || '2111-11-11');
+    window.showLicenseForm = () => {
+        const licenseInfo = JSON.parse(localStorage.getItem('licenseInfo')) || {};
+        $('#licenseeName').val(licenseInfo.licenseeName || '');
+        $('#assigneeName').val(licenseInfo.assigneeName || '');
+        $('#expiryDate').val(licenseInfo.expiryDate || '2111-11-11');
         $('#mask, #form').show();
     };
 
     // Function to show VM options
-    window.showVmoptins = function () {
-        var text = "-javaagent:/(Your Path)/ja-netfilter/ja-netfilter.jar\n" +
-        "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED\n" +
-        "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED";
+    window.showVmoptins = () => {
+        const text = `-javaagent:/(Your Path)/ja-netfilter/ja-netfilter.jar
+--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED
+--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED`;
         copyText(text)
             .then((result) => {
                 alert(result);
@@ -43,18 +43,18 @@ $(document).ready(function() {
     };
 
     // Function to copy license
-    window.copyLicense = async function (e) {
+    window.copyLicense = async (e) => {
         while (localStorage.getItem('licenseInfo') === null) {
             $('#mask, #form').show();
             await new Promise(r => setTimeout(r, 1000));
         }
-        let licenseInfo = JSON.parse(localStorage.getItem('licenseInfo'));
-        let productCode = $(e).closest('.card').data('productCodes');
-        let data = {
-            "licenseName": licenseInfo.licenseeName,
-            "assigneeName": licenseInfo.assigneeName,
-            "expiryDate": licenseInfo.expiryDate,
-            "productCode": productCode,
+        const licenseInfo = JSON.parse(localStorage.getItem('licenseInfo'));
+        const productCode = $(e.target).closest('.card').data('productCodes');
+        const data = {
+            licenseName: licenseInfo.licenseeName,
+            assigneeName: licenseInfo.assigneeName,
+            expiryDate: licenseInfo.expiryDate,
+            productCode: productCode,
         };
         $.post('/generateLicense', JSON.stringify(data))
             .then(response => {
@@ -68,23 +68,20 @@ $(document).ready(function() {
             });
     };
 
-// Function to copy text to clipboard
+    // Function to copy text to clipboard
     const copyText = async (val) => {
         if (navigator.clipboard && navigator.permissions) {
             return navigator.clipboard.writeText(val);
         } else {
-            console.log(val);
-            const textArea = document.createElement('textarea')
-            textArea.value = val
-            // 使text area不在viewport，同时设置不可见
-            document.body.appendChild(textArea)
-            textArea.focus()
-            textArea.select()
+            const textArea = document.createElement('textarea');
+            textArea.value = val;
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
             return new Promise((res, rej) => {
-                document.execCommand('copy') ? res() : rej()
-                textArea.remove()
-            })
+                document.execCommand('copy') ? res() : rej();
+                textArea.remove();
+            });
         }
     };
-
 });
